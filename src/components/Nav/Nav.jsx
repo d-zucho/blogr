867b2c35631c2c5f0c8b97ff0c98d0d86101a303
D_Zucho <danny.nico.dev@outlook.com>
2023-07-14 04:20:53 -0700

    fix nav and drop down; simplify dropdown code

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,59 +6,56 @@ function Nav() {
   return (
     <div className='navbar-container'>
       <img src={logo} alt='Logo' />
-      <nav className='links'>
-        <div className='link-container dropdown'>
-          <a href='#' className='link'>
-            Product
-          </a>
-          <img src={downArrow} alt='' />
-          <div className='dropdown-items'>
-            <a className='item' href='#'>
-              Overview
-            </a>
-            <a className='item' href='#'>
-              Pricing
-            </a>
-            <a className='item' href='#'>
-              Marketplace
-            </a>
-          </div>
-        </div>
-        <div className='link-container'>
-          <a href='#' className='link'>
-            Company
-          </a>
-          <img src={downArrow} alt='' />
-          <div className='dropdown-items'>
-            <a className='item' href='#'>
-              Overview
-            </a>
-            <a className='item' href='#'>
-              Pricing
-            </a>
-            <a className='item' href='#'>
-              Marketplace
-            </a>
-          </div>
-        </div>
-        <div className='link-container'>
-          <a href='#' className='link'>
-            Contact
-          </a>
-          <img src={downArrow} alt='' />
-          <div className='dropdown-items'>
-            <a className='item' href='#'>
-              Overview
-            </a>
-            <a className='item' href='#'>
-              Pricing
-            </a>
-            <a className='item' href='#'>
-              Marketplace
-            </a>
-          </div>
-        </div>
+      <nav role='navigation' className='links'>
+        <ul className='links-container'>
+          <li className='link-menu'>
+            <a href='#'>Product</a>
+            <img src={downArrow} alt='' />
+            <ul className='dropdown'>
+              <li>
+                <a href='#'>Overview</a>
+              </li>
+              <li>
+                <a href='#'>Pricing</a>
+              </li>
+              <li>
+                <a href='#'>MarketPlace</a>
+              </li>
+            </ul>
+          </li>
+          <li className='link-menu'>
+            <a href='#'>Company</a>
+            <img src={downArrow} alt='' />
+            <ul className='dropdown'>
+              <li>
+                <a href='#'>Overview</a>
+              </li>
+              <li>
+                <a href='#'>Pricing</a>
+              </li>
+              <li>
+                <a href='#'>MarketPlace</a>
+              </li>
+            </ul>
+          </li>
+          <li className='link-menu'>
+            <a href='#'>Contact</a>
+            <img src={downArrow} alt='' />
+            <ul className='dropdown'>
+              <li>
+                <a href='#'>Overview</a>
+              </li>
+              <li>
+                <a href='#'>Pricing</a>
+              </li>
+              <li>
+                <a href='#'>MarketPlace</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </nav>
+
       <div className='login-container'>
         <a href='#'>Login</a>
         <button className='btn signUp'>Sign Up</button>
